f8841c7502ad0dad356ee9e150db1ee9204efc26
Robert Putt <robe8437@Roberts-MacBook-Pro.local>
2021-08-09 16:44:05 +0100

    Add ping capability

--- hotspot_helper/views.py
+++ hotspot_helper/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from flask import jsonify
+from flask import request
 
 from hotspot_helper.miner_client import get_peerbook
+from hotspot_helper.miner_client import do_ping
 
 
 VIEWS = Blueprint('views', __name__)
@@ -9,7 +11,17 @@ VIEWS = Blueprint('views', __name__)
 
 @VIEWS.route('/ping', methods=['POST'])
 def ping():
-    return 'Not yet implemented', 501
+    try:
+        data = request.get_json()
+        addr = data.get('addr')
+    except Exception:
+        return 'Bad Request', 400
+
+    if not addr:
+        return 'Bad Request', 400
+
+    result = do_ping(addr)
+    return jsonify(result)
 
 
 @VIEWS.route('/connect', methods=['POST'])
